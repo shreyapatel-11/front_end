771e48797ecbfa897622adebac51eb218f2712aa
shreyapatel-11 <shr1390pate@gmail.com>
2022-01-20 11:01:15 +0530

    Initial commit.

--- angular/src/app/aside/aside.component.ts
+++ angular/src/app/aside/aside.component.ts
@@ -3,10 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
-  styleUrls: ['./aside.component.scss']
+  styleUrls: ['./aside.component.scss'] 
 })
 export class AsideComponent implements OnInit {
 
+  name : string='Shreya';
   constructor() { }
 
   ngOnInit(): void {
--- angular/src/app/header/header.component.ts
+++ angular/src/app/header/header.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  
+  name: string='header';
   constructor() { }
 
   ngOnInit(): void {
