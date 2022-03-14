52028a9ba1db7adbda89ec9fc00414ab4a00d900
shreyapatel-13 <shr1390patel@gmail.com>
2022-03-14 08:38:39 +0530

    javaScript string and array method

--- JavaScript/assets/javaScript/array.js
+++ JavaScript/assets/javaScript/array.js
@@ -60,31 +60,92 @@ let array1 = [
         company: "1Rivet"
     },
 ];
-marks = [80,66,78,56,79,90];
+
+
+// console.log("replace 1rivet to google: ",array1.replace("1Rivet","Google"))
+// -------------------------------------------------------------------------------------------
+marks = [80,66,78,56,79];
+// key and value pair
+for( x of marks.entries()){
+    console.log("entries",x)
+}
+// keys
+console.log("keys: ", Object.keys(marks))
 // findindex
-console.log("findIndex",marks.findIndex(x => x==90));
+console.log("findIndex: ",marks.findIndex(x => x==90));
 
 // map
-console.log("map",marks.map((x) => x/2));
+console.log("map: ",marks.map((x) => x/2));
 
 const isFrom1Rivet = (person) => person.company === "1Rivet";
 const isNotFrom1Rivet = (person) => person.company != "1Rivet";
 
-console.log("people from 1Rivet:- ",array1.filter(isFrom1Rivet));
+console.log("incluses",marks.includes(78,3))
+// forEach
+let result= "";
+function empName(index, name){
+    result += index + ": " + name + "<br>" 
+}
 
+// sort()
+console.log("sort() array: ",marks.sort());
+console.log("foreach: ",marks.forEach(empName));
+console.log("filter : people from 1Rivet:- ",array1.filter(isFrom1Rivet));
+console.log("find : ",array1.find(isFrom1Rivet));
+console.log("findIndex : ",array1.findIndex(isNotFrom1Rivet));
 
 // every return boolean
-console.log("people from 1Rivet:- ", array1.every(isFrom1Rivet));
+console.log("every: people from 1Rivet:- ", array1.every(isFrom1Rivet));
+console.log("some(): people from 1Rivet:- ", array1.some(isFrom1Rivet));
 // console.log("people not from 1Rivet:- ", array1.filter(isNotFrom1Rivet));
 // console.log(array1);
 
-const num = [1, 2, 3, 4, 5, 1];
-const number = [2, 3, 4]
+// --------------------------------------------------------------------------
+const num = [1, 2, 3, 1, 4, 5, 2];
+const number = [];
+
+// reduce()
+function getSum(sum, no){
+    return sum + no;
+}
+console.log("reduce", num.reduce(getSum))
+
+// toString()
+console.log("toString(): ", num.toString());
+
+// valueOf() returns the array itself:
+const copyNum = console.log("valueOf(): ", num.valueOf());
+
+// reverse()
+console.log("reverse array: ",num.reverse());
+
+// slice()
+console.log("slice(): ", num.slice(3,6))
+
+// let sum = 0;
+// function sumNum(){
+//     for(let i=0; i<=num.length; i++){
+//         sum = sum + i;
+//     }
+//     return sum;
+// }
+// console.log("reduce: ", num.reduce(sumNum))
+console.log("join",num.join(" & "))
+console.log("indexOf: ", num.indexOf(1,2))
+console.log("lastIndexOf: ", num.lastIndexOf(2))
+
+console.log("copyWithin",num.copyWithin(number));
+console.log("copyWithin",num.copyWithin(2,0))
+
+// entries
+// console.log(num.entries());
+
 console.log("fill: ",num.fill(8, 2));
 
 // chech array or not return boolean value
 console.log("isArray",Array.isArray(num))
 
+// ----------------------------------------------------------------------------------------------
 // splice
 const list = ["January","March","April","May"];
 console.log("list",list);
@@ -100,6 +161,10 @@ console.log("unshift",list.unshift("Jan"));
 
 // slice() method returns selected elements in an array, as a new array.
 console.log("slice",list.splice(1, 3)); 
+
+// ----------------------------------------------------------------------------------------------------
+let text = "HiiHello"
+console.log("Create array from string: ",Array.from(text))
 
 let array2 = [
     {
@@ -166,9 +231,6 @@ let array2 = [
 
 
 // concat 2 array
-const array3 = console.log(array1.concat(array2));
+const array3 = console.log("concat 2 array",array1.concat(array2));
 
-// key and value pair
-for( x of array1.entries()){
-    console.log(x)
-}
+
